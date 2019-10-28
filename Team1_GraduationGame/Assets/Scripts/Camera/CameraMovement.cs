5c997e199677f23918b2fbb45ee481f4e26d3dbf
Jannik15 <jneerd15@student.aau.dk>
2019-10-28 15:37:26 +0100

    Fixed camera height manipulation based on player z position

--- Team1_GraduationGame/Assets/Scripts/Camera/CameraMovement.cs
+++ Team1_GraduationGame/Assets/Scripts/Camera/CameraMovement.cs
@@ -6,6 +6,8 @@ public class CameraMovement : MonoBehaviour
 {
     [Tooltip("If Camera Target is not set, object with tag \"CamLookAt\" will be used instead. \n\"Player\" will be used as a fallback.")]
     public Transform camTarget;
+    [Tooltip("If Player Target is not set, object with tag \"Player\" will be used instead.")]
+    public Transform player;
     [Tooltip("If Camera Rail is not set, object with tag \"RailCamera\" will be used instead.")]
     public Transform camRail;
     [Tooltip("This value is used to determine the height when the target is far away.")]
@@ -14,16 +16,22 @@ public class CameraMovement : MonoBehaviour
 
     void Start()
     {
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player").transform;
         if (camTarget == null)
+        {
             camTarget = GameObject.FindGameObjectWithTag("CamLookAt").transform;
+            if (camTarget == null)
+                camTarget = player;
+        }
         if (camRail == null)
             camRail = GameObject.FindGameObjectWithTag("RailCamera").transform;
     }
 
     void Update()
     {
-        heightIncrease = Vector3.Distance(camTarget.position, new Vector3(camTarget.position.x, camRail.position.y, camRail.position.z)) * 0.2f;
+        heightIncrease = Vector3.Distance(player.position, new Vector3(player.position.x, camRail.position.y, camRail.position.z)) * heightDistanceFactor;
         transform.position = new Vector3(camTarget.position.x, camRail.position.y + heightIncrease, camRail.position.z);
-        transform.LookAt(camTarget);
+        transform.LookAt(camTarget); // TODO: Apply smoothing to the LookAt
     }
 }

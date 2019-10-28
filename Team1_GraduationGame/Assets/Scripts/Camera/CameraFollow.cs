0ad0efbb7ad4bf5743f637e26dbe40477e9beaee
Jannik15 <jneerd15@student.aau.dk>
2019-10-28 15:27:13 +0100

    Finished Camera on rails system - need to smoothen the LookAt
    Will merge with development

--- Team1_GraduationGame/Assets/Scripts/Camera/CameraFollow.cs
+++ Team1_GraduationGame/Assets/Scripts/Camera/CameraFollow.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    public Transform cam;
+    public Transform camRail;
+    [Tooltip("This value is used to determine the height when the target is far away.")]
+    public float heightDistanceFactor = 0.2f;
+    private float heightIncrease;
 
     void Start()
     {
         if (player == null)
             player = GameObject.FindGameObjectWithTag("Player").transform;
-        if (cam == null)
-            cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        if (camRail == null)
+            camRail = GameObject.FindGameObjectWithTag("RailCamera").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, cam.position.y, cam.position.z);
+        heightIncrease = Vector3.Distance(player.position, new Vector3(player.position.x, camRail.position.y, camRail.position.z)) * 0.2f;
+        transform.position = new Vector3(player.position.x, camRail.position.y + heightIncrease, camRail.position.z);
     }
 }
---
+++ Team1_GraduationGame/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Tooltip("If Camera Target is not set, object with tag \"CamLookAt\" will be used instead. \n\"Player\" will be used as a fallback.")]
+    public Transform camTarget;
+    [Tooltip("If Camera Rail is not set, object with tag \"RailCamera\" will be used instead.")]
+    public Transform camRail;
+    [Tooltip("This value is used to determine the height when the target is far away.")]
+    public float heightDistanceFactor = 0.2f;
+    private float heightIncrease;
+
+    void Start()
+    {
+        if (camTarget == null)
+            camTarget = GameObject.FindGameObjectWithTag("CamLookAt").transform;
+        if (camRail == null)
+            camRail = GameObject.FindGameObjectWithTag("RailCamera").transform;
+    }
+
+    void Update()
+    {
+        heightIncrease = Vector3.Distance(camTarget.position, new Vector3(camTarget.position.x, camRail.position.y, camRail.position.z)) * 0.2f;
+        transform.position = new Vector3(camTarget.position.x, camRail.position.y + heightIncrease, camRail.position.z);
+        transform.LookAt(camTarget);
+    }
+}

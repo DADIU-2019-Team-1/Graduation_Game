0b40b627b9cafe2efbfe90756320e344e23e4d1c
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-10-28 10:22:16 +0100

    Joystick boundary and left side only fixed

--- Team1_GraduationGame/Assets/Scripts/Player/Movement.cs
+++ Team1_GraduationGame/Assets/Scripts/Player/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody playerRB;
-    private bool touchStart = false;
+    private bool touchStart = false, canMove = false;
     private Vector3 initTouchPos;
     private Vector3 currTouchPos;
 
@@ -41,8 +41,12 @@ public class Movement : MonoBehaviour
 
             stickLimit.transform.position = Input.mousePosition; /* initTouchPos * -1; */
             stick.transform.position = Input.mousePosition;  /* initTouchPos * -1; */
-            stick.gameObject.SetActive(true);
-            stickLimit.gameObject.SetActive(true);
+            if(stickLimit.transform.position.x < Screen.width/2) {
+                canMove = true;
+                stick.gameObject.SetActive(true);
+                stickLimit.gameObject.SetActive(true);                
+            }
+
         }
         if(Input.GetMouseButton(0)) {
             
@@ -55,16 +59,18 @@ public class Movement : MonoBehaviour
     }
 
     void FixedUpdate() {
-        if(touchStart) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(touchStart && canMove){
+/*             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if(Physics.Raycast(ray, out hit)) {
                 //Debug.Log("Hit " + hit.transform.gameObject);
                 joystickPos = new Vector3(hit.point.x, hit.point.y, 0);
-            }
+            } */
             Vector3 offset = new Vector3(currTouchPos.x-initTouchPos.x, 0, currTouchPos.y - initTouchPos.y);
             Vector3 direction = Vector3.ClampMagnitude(offset, 1.0f);
             float dragDist = Vector2.Distance(stick.transform.position, stickLimit.transform.position);
+            Vector2 joyDiff = Input.mousePosition - stickLimit.transform.position;
+            joyDiff = Vector2.ClampMagnitude(joyDiff, radius.value);
             //Debug.Log("Drag Distance is: " + dragDist + "Radius is: " + radius.value);
             if(dragDist < radius.value * 0.25) {
                 movePlayer(direction * -1, sneakSpeed.value);
@@ -83,11 +89,12 @@ public class Movement : MonoBehaviour
                 
             //movePlayer(direction * -1, movementSpeed.value);
             //if(Vector2.Distance(Input.mousePosition, stickLimit.transform.position) < dragDist)
-            stick.transform.position = Input.mousePosition /* Vector2.ClampMagnitude(Input.mousePosition, 1100) */;/* new Vector3(initTouchPos.x + direction.x, initTouchPos.z + direction.z) * -1; */
+            stick.transform.position = joyDiff + new Vector2(stickLimit.transform.position.x, stickLimit.transform.position.y);/* Input.mousePosition */ /* Vector2.ClampMagnitude(Input.mousePosition, 1100) */;/* new Vector3(initTouchPos.x + direction.x, initTouchPos.z + direction.z) * -1; */
         }
         else {
             stick.gameObject.SetActive(false);
             stickLimit.gameObject.SetActive(false);
+            canMove = false;
         }
     }
 

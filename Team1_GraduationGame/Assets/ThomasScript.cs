64fbee111c9e5c4cb1a6144b5ec92ad8c2295bee
noesmos <tnm@live.dk>
2019-10-30 15:37:36 +0100

    obstacle course for Howalt done

---
+++ Team1_GraduationGame/Assets/ThomasScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThomasScript : MonoBehaviour
+{
+
+    GameObject player;
+    public Transform resetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.position = resetPos.position;
+        }
+    }
+}

c8551b89c15941dbedddc9c591aaf192b957ff81
Jakob <jakob.hust@gmail.com>
2019-10-30 09:05:18 +0100

    update

---
+++ Team1_GraduationGame/Assets/Scripts/Enemies/WayPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team1_GraduationGame.Enemies
+{
+    [ExecuteInEditMode]
+    public class WayPoint : MonoBehaviour
+    {
+        private GameObject parentEnemy;
+
+        void OnDestroy()
+        {
+
+        }
+    }
+
+}

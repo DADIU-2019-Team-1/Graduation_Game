e1643a5d999b2961f88f77d9ef42f839094d5245
Jannik15 <jneerd15@student.aau.dk>
2019-11-04 13:53:21 +0100

    Finished adding offset array to the rail (seperate script) - need better way of updating it during edit

--- Team1_GraduationGame/Assets/Scripts/Camera/CameraLook.cs
+++ Team1_GraduationGame/Assets/Scripts/Camera/CameraLook.cs
@@ -1,15 +1,35 @@
-﻿using UnityEngine;
+﻿// Code owner: Jannik Neerdal
+using UnityEngine;
+using System.Collections.Generic;
+using Cinemachine;
 
+[ExecuteInEditMode]
 public class CameraLook : MonoBehaviour
 {
+    // --- Inspector
     [Tooltip("If no player is defined, the GameObject with the tag \"Player\" will be used.")]
     public Transform player;
-    public Vector3 offset = new Vector3(0.0f,0.0f,0.0f);
-    [Tooltip("Control the amount that the players direction influences the camera.")] [Range(0.0f,1.0f)]
-    [SerializeField] private float lookDirFactor = 0.5f;
 
+    [Tooltip("If no Input is defined, the object with the script \"CameraMovement\" attached will be used.")]
+    public CameraMovement camMovement;
+
+    [Tooltip("The default offset that will always be added")]
+    public Vector3 defaultOffset = new Vector3(0.0f,0.0f,0.0f);
+
+    [Tooltip("Control the amount that the players direction influences the camera.")]
+    [Range(0.0f, 1.0f)] [SerializeField] private float lookDirFactor = 0.5f;
+
+    [Tooltip("Determines how long it should take for the offset to update. 1 is instant.")]
+    [Range(0.01f, 1.0f)] [SerializeField] private float offsetLerpTime = 0.05f;
+
+    public CameraOffset[] offsetTrack;
+
+    // --- Hidden
+    [HideInInspector] public Vector3 camTarget;
     private Movement playerMovement;
     private Vector3 moveDir;
+    private Vector3 offset;
+    private CinemachineSmoothPath cmPath;
 
     void Start()
     {
@@ -20,14 +40,44 @@ public class CameraLook : MonoBehaviour
             playerMovement = player.GetComponent<Movement>();
         else
             Debug.LogError("The player does not have a movement script attached!", player);
+
+        if (camMovement == null)
+            camMovement = FindObjectOfType<CameraMovement>();
+
+        if (GetComponent<CinemachineSmoothPath>() != null)
+            cmPath = GetComponent<CinemachineSmoothPath>();
+        else
+            Debug.LogError("The DollyTrack is missing its track!", gameObject);
+
+        if (cmPath.m_Waypoints.Length != offsetTrack.Length)
+        {
+            CameraOffset[] temp = new CameraOffset[offsetTrack.Length];
+            for (int i = 0; i < offsetTrack.Length; i++)
+            {
+                temp[i] = new CameraOffset(offsetTrack[i].GetPos(), offsetTrack[i].GetFOV());
+            }
+            offsetTrack = new CameraOffset[cmPath.m_Waypoints.Length];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                offsetTrack[i] = temp[i]; // TODO: Move to OnValidate or similar
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(camTarget, 1.0f);
     }
 
     void Update()
     {
-        if (player != null)
+        if (playerMovement != null)
         {
             moveDir = player.forward * playerMovement.GetSpeed() * lookDirFactor;
-            transform.position = player.position + offset + moveDir;
         }
+        offset = Vector3.Lerp(offset, (defaultOffset + offsetTrack[camMovement.previousTrackIndex].GetPos() + offsetTrack[camMovement.nextTrackIndex].GetPos()) / 2, offsetLerpTime);
+        camTarget = player.position + offset + moveDir;
     }
 }

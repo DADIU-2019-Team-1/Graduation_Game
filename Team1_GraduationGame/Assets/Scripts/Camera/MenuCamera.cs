5d52f7298503460e6c667812be6f9c266202c64f
Jannik15 <jneerd15@student.aau.dk>
2019-11-11 16:34:47 +0100

    Fixed menu camera starting game

--- Team1_GraduationGame/Assets/Scripts/Camera/MenuCamera.cs
+++ Team1_GraduationGame/Assets/Scripts/Camera/MenuCamera.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class MenuCamera : MonoBehaviour
 {
     [SerializeField] private CinemachineSmoothPath _rail;
     [SerializeField] private CinemachineVirtualCamera railCamera;
     [SerializeField] private Transform[] lookAtTargets;
-    [SerializeField] private GameObject[] objectsToActive;
+    [SerializeField] private CanvasGroup[] menuObjectsToSetActivate;
+    [SerializeField] private PlayableDirector startingTimeline;
     [SerializeField] private FloatReference camLookSpeed, camMoveTime;
     [SerializeField] private FloatReference activateMenuThreshold, waitBeforeMoving;
     [SerializeField] [Range(0.01f, 1.0f)] private float fadeAmount = 0.05f;
     private int currentTargetIndex, railIndex = 0;
     private Quaternion targetRotation;
     private Vector3 camMovement;
-    private bool _move;
+    private bool _move, _startingGame;
 
     void Start()
     {
@@ -23,66 +25,75 @@ public class MenuCamera : MonoBehaviour
             _rail = FindObjectOfType<CinemachineSmoothPath>();
         if (railCamera == null)
             railCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        if (startingTimeline == null)
+            startingTimeline = FindObjectOfType<PlayableDirector>();
     }
 
     void LateUpdate()
     {
-        // Object activation
-        if (Vector3.Distance(transform.position, _rail.m_Waypoints[railIndex].position + _rail.transform.position) <= activateMenuThreshold.value)
+        if (!_startingGame)
         {
-            StopCoroutine(WaitForTextFade());
-            for (int i = 0; i < objectsToActive.Length; i++)
+            // Object activation
+            if (Vector3.Distance(transform.position, _rail.m_Waypoints[railIndex].position + _rail.transform.position) <= activateMenuThreshold.value)
             {
-                if (i == currentTargetIndex)
+                StopCoroutine(WaitForTextFade());
+                for (int i = 0; i < menuObjectsToSetActivate.Length; i++)
                 {
-                    objectsToActive[i].SetActive(true);
-                    if (objectsToActive[i].GetComponent<CanvasGroup>())
-                        objectsToActive[i].GetComponent<CanvasGroup>().alpha += fadeAmount;
-                }
-                else
-                {
-                    if (objectsToActive[i].GetComponent<CanvasGroup>())
+                    if (i == currentTargetIndex)
                     {
-                        objectsToActive[i].GetComponent<CanvasGroup>().alpha -= fadeAmount;
-                        if (objectsToActive[i].GetComponent<CanvasGroup>().alpha < fadeAmount)
-                            objectsToActive[i].SetActive(false);
+                        menuObjectsToSetActivate[i].gameObject.SetActive(true);
+                        menuObjectsToSetActivate[i].alpha += fadeAmount;
                     }
                     else
                     {
-                        objectsToActive[i].SetActive(false);
+                        menuObjectsToSetActivate[i].alpha -= fadeAmount;
+                        if (menuObjectsToSetActivate[i].alpha < fadeAmount)
+                            menuObjectsToSetActivate[i].gameObject.SetActive(false);
                     }
                 }
+            }
+            else
+            {
+                StartCoroutine(WaitForTextFade());
+                for (int i = 0; i < menuObjectsToSetActivate.Length; i++)
+                {
+                    menuObjectsToSetActivate[i].alpha -= fadeAmount;
+                    if (menuObjectsToSetActivate[i].alpha < fadeAmount)
+                        menuObjectsToSetActivate[i].gameObject.SetActive(false);
+                }
+            }
+
+            if (_move)
+            {
+                // Position update // TODO: Optimize so not only smoothening x axis (or smoothening all)
+                transform.position = new Vector3(transform.position.x, railCamera.transform.position.y, railCamera.transform.position.z);
+                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(_rail.m_Waypoints[railIndex].position.x + _rail.transform.position.x, transform.position.y, transform.position.z),
+                    ref camMovement, camMoveTime.value * Time.deltaTime);
+
+                // Rotation update
+                targetRotation = lookAtTargets[currentTargetIndex].position - transform.position != Vector3.zero
+                    ? Quaternion.LookRotation(lookAtTargets[currentTargetIndex].position - transform.position) : Quaternion.identity;
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, camLookSpeed.value * Time.deltaTime);
             }
         }
         else
         {
-            StartCoroutine(WaitForTextFade());
-            for (int i = 0; i < objectsToActive.Length; i++)
+            bool startGame = false;
+            for (int i = 0; i < menuObjectsToSetActivate.Length; i++)
             {
-                if (objectsToActive[i].GetComponent<CanvasGroup>())
+                if (menuObjectsToSetActivate[i].gameObject.activeSelf)
                 {
-                    objectsToActive[i].GetComponent<CanvasGroup>().alpha -= fadeAmount;
-                    if (objectsToActive[i].GetComponent<CanvasGroup>().alpha < fadeAmount)
-                        objectsToActive[i].SetActive(false);
-                }
-                else
-                {
-                    objectsToActive[i].SetActive(false);
+                    menuObjectsToSetActivate[i].alpha -= fadeAmount;
+                    if (menuObjectsToSetActivate[i].alpha < fadeAmount)
+                    {
+                        startGame = true;
+                    }
                 }
             }
-        }
-
-        if (_move)
-        {
-            // Position update // TODO: Optimize so not only smoothening x axis (or smoothening all)
-            transform.position = new Vector3(transform.position.x, railCamera.transform.position.y, railCamera.transform.position.z);
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(_rail.m_Waypoints[railIndex].position.x + _rail.transform.position.x, transform.position.y, transform.position.z),
-                ref camMovement, camMoveTime.value * Time.deltaTime);
-
-            // Rotation update
-            targetRotation = lookAtTargets[currentTargetIndex].position - transform.position != Vector3.zero
-                ? Quaternion.LookRotation(lookAtTargets[currentTargetIndex].position - transform.position) : Quaternion.identity;
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, camLookSpeed.value * Time.deltaTime);
+            if (startGame)
+            {
+                StartGame();
+            }
         }
     }
 
@@ -95,19 +106,22 @@ public class MenuCamera : MonoBehaviour
 
     public void StartGame()
     {
-        Debug.Log("Starting game!");
-        StartCoroutine(StartGameCoroutine());
+        if (_startingGame)
+        {
+            startingTimeline.Play();
+        }
+        _startingGame = true;
     }
 
     public IEnumerator StartGameCoroutine()
     {
         while (true)
         {
-            for (int i = 0; i < objectsToActive.Length; i++)
+            for (int i = 0; i < menuObjectsToSetActivate.Length; i++)
             {
-                if (objectsToActive[i].GetComponent<CanvasGroup>())
+                if (menuObjectsToSetActivate[i].GetComponent<CanvasGroup>())
                 {
-                    objectsToActive[i].GetComponent<CanvasGroup>().alpha -= fadeAmount;
+                    menuObjectsToSetActivate[i].GetComponent<CanvasGroup>().alpha -= fadeAmount;
                 }
             }
             yield return new WaitForSeconds(5.0f);

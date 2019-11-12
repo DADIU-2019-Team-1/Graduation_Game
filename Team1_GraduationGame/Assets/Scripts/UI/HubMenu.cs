9f962be8de33005fd503465ff44fcc47184d89f9
Jannik15 <jneerd15@student.aau.dk>
2019-11-12 14:26:48 +0100

    New ui and menu system

---
+++ Team1_GraduationGame/Assets/Scripts/UI/HubMenu.cs
@@ -0,0 +1,37 @@
+﻿// Code Owner: Jannik Neerdal
+using System;
+using UnityEngine;
+
+public class HubMenu : MonoBehaviour
+{
+    public event Action startGameEvent;
+    public event Action continueGameEvent; // TODO: Continue is not yet implemented 
+    public event Action<int> menuChangeEvent;
+    public event Action<string> languageChangeEvent; // TODO: Localization is not yet implemented
+
+    public void ChangeLanguage(string s)
+    {
+        languageChangeEvent?.Invoke(s);
+    }
+    public void ChangeMenu(int i)
+    {
+        menuChangeEvent?.Invoke(i);
+    }
+    public void StartGame()
+    {
+        startGameEvent?.Invoke();
+    }
+    public void ContinueGame()
+    {
+        continueGameEvent?.Invoke();
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}

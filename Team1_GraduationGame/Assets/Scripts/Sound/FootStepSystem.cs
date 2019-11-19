d55c446df6eec2cbe00776f5155471931ebb5170
Jakob <jakob.hust@gmail.com>
2019-11-19 14:49:54 +0100

    First version of working footstep system working

--- Team1_GraduationGame/Assets/Scripts/Sound/FootStepSystem.cs
+++ Team1_GraduationGame/Assets/Scripts/Sound/FootStepSystem.cs
@@ -7,7 +7,6 @@
 
     public class FootStepSystem : MonoBehaviour
     {
-
         public string[] materialTypes;
         public IntEvent footStepEvent;
 
@@ -28,10 +27,12 @@
                 if (thisTerrain.terrainData.terrainLayers.Length > 0)
                 {
                     FootStepRaise(thisTerrain.terrainData.terrainLayers[0].diffuseTexture.ToString());
-                    Debug.Log(thisTerrain.terrainData.terrainLayers[0].diffuseTexture);
                 }
             }
-
+            else if (col.gameObject.GetComponent<Collider>() != null)
+            {
+                FootStepRaise(col.gameObject.name);
+            }
         }
 
         private void FootStepRaise(string matLayerName)
@@ -43,7 +44,7 @@
 
             for (int i = 0; i < materialTypes.Length; i++)
             {
-                if (materialTypes[i].Contains(matLayerName))
+                if (matLayerName.Contains(materialTypes[i]))
                 {
                     footStepEvent?.Raise(i);
                     return;

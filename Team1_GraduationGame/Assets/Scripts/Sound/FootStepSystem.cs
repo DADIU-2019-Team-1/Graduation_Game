6b8c8d4d86f47c7710c16967754b51ce8aacae32
Jakob <jakob.hust@gmail.com>
2019-11-19 10:23:29 +0100

    Added foot step script

---
+++ Team1_GraduationGame/Assets/Scripts/Sound/FootStepSystem.cs
@@ -0,0 +1,29 @@
+﻿namespace Team1_GraduationGame.Sound
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class FootStepSystem : MonoBehaviour
+    {
+
+        private void Awake()
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision col)
+        {
+            Debug.Log(col.gameObject.name);
+
+            if (col.gameObject.GetComponent<Terrain>() != null)
+            {
+                Terrain thisTerrain = col.gameObject.GetComponent<Terrain>();
+                Debug.Log(thisTerrain.terrainData.GetAlphamapTexture(0).name);
+            }
+
+        }
+
+    }
+}
+

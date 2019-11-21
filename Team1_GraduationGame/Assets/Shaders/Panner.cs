fa64014fb93863d180920d174850819d707cceeb
lord001202 <42942404+lord001202@users.noreply.github.com>
2019-11-21 23:59:08 +0100

    New idea for start

--- Team1_GraduationGame/Assets/Shaders/Panner.cs
+++ Team1_GraduationGame/Assets/Shaders/Panner.cs
@@ -6,7 +6,8 @@ public class Panner : MonoBehaviour
 {
     // Scroll main texture based on time
 
-    float scrollSpeed = 0.5f;
+    public float scrollSpeedX = 0.5f;
+    public float scrollSpeedY = 0.5f;
     Renderer rend;
 
     void Start()
@@ -16,7 +17,8 @@ public class Panner : MonoBehaviour
 
     void Update()
     {
-        float offset = Time.time * scrollSpeed;
-        rend.material.SetTextureOffset("_EmissionMap", new Vector2(offset, 0));
+        float offsetX = Time.time * scrollSpeedX;
+        float offsetY = Time.time * scrollSpeedY;
+        rend.material.SetTextureOffset("_EmissionMap", new Vector2(offsetX, offsetY));
     }
 }

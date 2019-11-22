4c137cb2cd09fd23c7653d57e8249940f3811e9c
Jakob <jakob.hust@gmail.com>
2019-11-22 09:37:35 +0100

    Fixed collider checker

---
+++ Team1_GraduationGame/Assets/Scripts/Sound/PlayerSoundManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Team1_GraduationGame.Sound
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PlayerSoundManager : MonoBehaviour
+    {
+
+    }
+}

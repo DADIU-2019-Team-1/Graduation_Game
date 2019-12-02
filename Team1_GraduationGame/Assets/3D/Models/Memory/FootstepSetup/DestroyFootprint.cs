beaceed26b8e0c043acf069a6eb345caa9babe9a
Jakob <jakob.hust@gmail.com>
2019-12-02 13:36:54 +0100

    Delayed start on event manager + sound manager

--- Team1_GraduationGame/Assets/3D/Models/Memory/FootstepSetup/DestroyFootprint.cs
+++ Team1_GraduationGame/Assets/3D/Models/Memory/FootstepSetup/DestroyFootprint.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class DestroyFootprint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     public void DestroyMe()
     {
         Destroy(gameObject);

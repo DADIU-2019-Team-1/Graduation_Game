b590834a0b3be6926d5af817aab630c74d2012bc
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-12-04 15:46:04 +0100

    Save now deletes at end credits, and it returns to main menu

--- Team1_GraduationGame/Assets/Scripts/UI/EndCredit.cs
+++ Team1_GraduationGame/Assets/Scripts/UI/EndCredit.cs
@@ -5,20 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class EndCredit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ResetSaves()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ChangeToStart()
-    {
-        SceneManager.LoadScene(0);
+        PlayerPrefs.SetInt("previousGame", 0);
     }
 }

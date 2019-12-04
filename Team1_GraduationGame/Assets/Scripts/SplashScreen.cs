58c3d21c128270ed1b30b833757a25b65fba60bc
Jannik15 <jneerd15@student.aau.dk>
2019-12-05 00:27:48 +0100

    Fixed quality buttons

--- Team1_GraduationGame/Assets/Scripts/SplashScreen.cs
+++ Team1_GraduationGame/Assets/Scripts/SplashScreen.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using Team1_GraduationGame.UI;
-using UnityEngine.Rendering;
 
 public class SplashScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
     private List<WhiteFadeController> fadeControllers;
     private bool _doOnce = false;
+
     void Start()
     {
+        // Set the Quality to the stored quality in player prefs
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("Quality"));
         fadeControllers = new List<WhiteFadeController>();
         fadeControllers.Add(GetComponent<WhiteFadeController>());
         fadeControllers.AddRange(GetComponentsInChildren<WhiteFadeController>());
@@ -21,10 +21,8 @@ public class SplashScreen : MonoBehaviour
                 fadeControllers[i].RaiseFadeEvent();
             }
         }
-            
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && fadeControllers[0] != null && UnityEngine.Rendering.SplashScreen.isFinished && !_doOnce)

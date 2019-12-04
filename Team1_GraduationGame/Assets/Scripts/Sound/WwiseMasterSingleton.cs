5aca32d689041cb9c1e43ed1902d2eabeb71f7e5
Jakob <jakob.hust@gmail.com>
2019-12-04 12:01:47 +0100

    Made Wwise Master singleton

---
+++ Team1_GraduationGame/Assets/Scripts/Sound/WwiseMasterSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WwiseMasterSingleton : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject[] _wwiseMasters = GameObject.FindGameObjectsWithTag("Wwise_Master");
+
+        if (_wwiseMasters != null)
+        {
+            if (_wwiseMasters.Length > 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}

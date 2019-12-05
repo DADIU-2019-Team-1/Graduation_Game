7a18c5ce17001641ad097dc08ac1fa8dd06e40f8
Jannik15 <jneerd15@student.aau.dk>
2019-12-05 15:03:38 +0100

    Fixed continue if there is no save (no longer interactable if previousGame is 0)

---
+++ Team1_GraduationGame/Assets/Scripts/UI/ContinueEnabler.cs
@@ -0,0 +1,18 @@
+﻿// Code Owner: Jannik Neerdal & Jakob Husted
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ContinueEnabler : MonoBehaviour
+{
+    private Button continueBtn;
+    void Start()
+    {
+        continueBtn = GetComponent<Button>();
+
+        if (PlayerPrefs.GetInt("previousGame") == 1)
+            continueBtn.interactable = true;
+        else
+            continueBtn.interactable = false;
+    }
+}
